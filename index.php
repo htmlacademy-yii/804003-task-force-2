f69b4a61dd0ad955b9ee0a65d7858a5efc510966
lozhkin <a.p.lozhkin@mail.ru>
2021-11-14 18:59:26 +0300

    задание 1.11. Камень судьбы - устанение замечаний

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once('Task.php');
+
+$task = new Task(25, 44, "status_inwork");
+
+echo "Текущий статус:  ";
+print_r($task->getStatus());
+echo "<br>";
+
+echo "все доступные статусы: ";
+print_r($task->getAllStatuses());
+echo "<br>";
+
+echo "все доступные действия: ";
+print_r($task->getAvailableActions());
+echo "<br>";
+
+echo "статус, в который перейдёт задание после выполнения конкретного действия: ";
+print_r($task->getNextStatus('action_performed'));
+echo "<br>";
+
+echo "список доступных действий в текущем статусе :";
+$task->getAvailableStatuses();
+echo "<br>";
--- task.php
+++ task.php
@@ -2,23 +2,23 @@
 
 class Task
 {
-    public const STATUS_NEW = 'status_new';
-    public const STATUS_CANCELED = 'status_canceled';
-    public const STATUS_INWORK = 'status_inwork';
-    public const STATUS_DONE = 'status_done';
-    public const STATUS_FAILED = 'status_failed';
+    private const STATUS_NEW = 'status_new';
+    private const STATUS_CANCELED = 'status_canceled';
+    private const STATUS_INWORK = 'status_inwork';
+    private const STATUS_DONE = 'status_done';
+    private const STATUS_FAILED = 'status_failed';
 
-    public const ACTION_CANCEL = 'action_cancel';
-    public const ACTION_RESPOND = 'action_respond';
-    public const ACTION_PERFORMED = 'action_performed';
-    public const ACTION_REFUSE = 'action_refuse';
+    private const ACTION_CANCEL = 'action_cancel';
+    private const ACTION_RESPOND = 'action_respond';
+    private const ACTION_PERFORMED = 'action_performed';
+    private const ACTION_REFUSE = 'action_refuse';
 
-    public $idApplicant;
-    public $idEmployer;
-    public $status;
+    private $idApplicant;
+    private $idEmployer;
+    private $status;
 
     // массив из доступных статусов
-    public $avaliabeStatus = 
+    private $avaliabeStatuses = 
     [
         self::STATUS_NEW => 'Новое',
         self::STATUS_CANCELED => 'Отменено',
@@ -28,7 +28,7 @@ class Task
     ];
 
     // массив из доступных действий
-    public $avaliabeActions =
+    private $avaliabeActions =
     [
         self::ACTION_CANCEL => 'Отменить',
         self::ACTION_RESPOND => 'Откликнуться',
@@ -37,7 +37,7 @@ class Task
     ];
 
     // массив, где ключ - выполняемое действие, значение - статус, в котороый перейдет задания после выполнения действия
-    public $statusAfterAction =
+    private $statusAfterAction =
     [
         self::ACTION_CANCEL => self::STATUS_CANCELED,
         self::ACTION_RESPOND => self::STATUS_INWORK,
@@ -46,19 +46,20 @@ class Task
     ];
 
     // массив, где ключ - возможный статус, значение - массив действий при данном статусе
-    public $actionDependentStatus =
+    private $actionDependentStatus =
     [
         self::STATUS_NEW => [self::ACTION_CANCEL, self::ACTION_RESPOND],
         self::STATUS_INWORK => [self::ACTION_PERFORMED, self::ACTION_REFUSE]
     ];
 
     // конструктор класса, принимает id заказчика и исполнителя, создает задание, присваивает статус Новое
-    public function __construct($idEmployer, $idApplicant)
+    public function __construct(int $idEmployer, int $idApplicant, string $status = self::STATUS_NEW)
     {
         $this->idApplicant = $idApplicant;
         $this->idEmployer = $idEmployer;
-        $this->status = self::STATUS_NEW;
+        $this->status = $status;
     }
+
     // метод getStatus определяет текущий статус задания
     public function getStatus()
     {
@@ -66,46 +67,29 @@ class Task
     }
 
     //метод getAvailableAction определяет список из всех возможных действий
-    public function getAvailableActions() {
-        foreach ($this->avaliabeActions as $k => $actions) {
-            echo $actions;
-        }
+    public function getAvailableActions() 
+    {
+        return $this->avaliabeActions;
     }
 
     //метод возвращает список доступных действий в текущем статусе;
-    public function getAvailableStatus() {
+    public function getAvailableStatuses() 
+    {
         $actions = $this->actionDependentStatus[$this->status];
         for ($i=0; $i < count($actions); $i++) {
             echo $this->avaliabeActions[$actions[$i]];
         }
     }
     
-    //метод getAvailableStatus определяет список из всех возможных статусов
-    public function getAvailableStatuses() {
-        foreach ($this->avaliabeStatus as $k => $status) {
-            echo $status;
-        }
+    //метод getAllStatuses определяет список из всех возможных статусов
+    public function getAllStatuses() 
+    {
+        return $this->avaliabeStatuses;
     }
 
     //метод возвращает имя статуса, в который перейдёт задание после выполнения конкретного действия;
-    public function getNextStatus(string $action) {
-        return $this->avaliabeStatus[$this->statusAfterAction[$action]];
+    public function getNextStatus(string $action) 
+    {
+        return $this->avaliabeStatuses[$this->statusAfterAction[$action]];
     }
 }
-
-$task = new Task(25, 44);
-echo "все доступные статусы: ";
-$task->getAvailableStatuses();
-echo "<br>";
-
-echo "все доступные действия: ";
-$task->getAvailableActions();
-echo "<br>";
-
-echo "статус, в который перейдёт задание после выполнения конкретного действия: ";
-print_r($task->getNextStatus('action_performed'));
-echo "<br>";
-
-echo "список доступных действий в текущем статусе :";
-$task->getAvailableStatus();
-echo "<br>";
